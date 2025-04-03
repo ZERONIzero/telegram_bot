e11a79cfbc57a45540b41468ccbf1cc9d8a6d4d5
ZeroNi <130401304+ZERONIzero@users.noreply.github.com>
2025-04-03 08:20:41 +0300

    Create command.js

---
+++ settings/command.js
@@ -0,0 +1,16 @@
+let commands = [
+    {
+        command: "start",
+        description : "Запуск бота"
+    },
+    {
+        command: "ref",
+        description: "Получить реферальную ссылку"
+    },
+    {
+        command: "about",
+        description: "Узнать об авторах"
+    }
+]
+
+module.exports = commands;
